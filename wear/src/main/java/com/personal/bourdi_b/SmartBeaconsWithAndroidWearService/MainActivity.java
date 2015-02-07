4741f6c6026d27dfb3bbbcb181591fb1bc072937
Benjamin Bourdin <benjamin.bourdin40@gmail.com>
2015-02-07 17:25:15 +0100

    Add a button which allows to start/stop the service, thus we do not have to kill it manually in a tasks manager.

--- wear/src/main/java/com/personal/bourdi_b/SmartBeaconsWithAndroidWearService/MainActivity.java
+++ wear/src/main/java/com/personal/bourdi_b/SmartBeaconsWithAndroidWearService/MainActivity.java
@@ -1,8 +1,12 @@
 package com.personal.bourdi_b.SmartBeaconsWithAndroidWearService;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +16,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, DiscoverBeaconsService.class));
+
+        final Button bt_stop_start_service = (Button) findViewById(R.id.start_stop_service);
+        bt_stop_start_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isMyServiceRunning(DiscoverBeaconsService.class)) {
+                    stopService(new Intent(MainActivity.this, DiscoverBeaconsService.class));
+                    bt_stop_start_service.setText(R.string.start_service);
+                } else {
+                    startService(new Intent(MainActivity.this, DiscoverBeaconsService.class));
+                    bt_stop_start_service.setText(R.string.stop_service);
+                }
+            }
+        });
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
